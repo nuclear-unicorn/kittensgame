f3021cd51cd5624f646fe5f1c7559401c756673a
Brent-Call <tnerbcall@comcast.net>
2026-06-26 21:37:56 -0400

    Improve embassy tooltips while in queue

--- js/jsx/queue.jsx.js
+++ js/jsx/queue.jsx.js
@@ -90,7 +90,18 @@ WQueueItem = React.createClass({
         if (!controllerAndModel) {
             return;
         }
-        UIUtils.attachTooltip(game, node, 0, 200, dojo.partial(ButtonModernHelper.getTooltipHTML, controllerAndModel.controller, controllerAndModel.model));
+        var controller = controllerAndModel.controller;
+        var model = controllerAndModel.model;
+        if (item.type === "embassies") {
+            //All embassy models have the same name, which is unhelpful in the context of the queue.
+            //It's safe to change the model object because of how initModel is implemented in core.js.
+            model.tooltipName = true; //Show name in tooltips
+            model.name = $I("trade.embassy.queue.item", [game.diplomacy.get(item.name).title]);
+        }
+
+        //Create tooltip
+        var tooltipHelperObj = item.type === "embassies" ? EmbassyButtonHelper : ButtonModernHelper;
+        UIUtils.attachTooltip(game, node, 0, 200, dojo.partial(tooltipHelperObj.getTooltipHTML, controller, model));
     },
 
     //Ask the game engine if this item is storage-limited
